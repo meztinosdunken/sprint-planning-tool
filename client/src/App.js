b534ff645f433e426fef8214e7213887ac0afaf2
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-30 01:03:50 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sprint Planning Tool</h1>
+      <p>Welcome to the Sprint Planning Tool. Let's plan your sprints effectively!</p>
+    </div>
+  );
+}
+
+export default App;
